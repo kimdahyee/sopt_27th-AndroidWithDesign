f1b2cdf39198ff825e4023164391e5651e93f3b9
YEJIN-LILY <gristar98@naver.com>
2020-11-20 22:25:24 +0900

    Update ProductFragment

---
+++ app/src/main/java/com/example/androidwithdesign/ProductViewHolder.kt
@@ -0,0 +1,9 @@
+package com.example.androidwithdesign
+
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ProductViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+
+}
