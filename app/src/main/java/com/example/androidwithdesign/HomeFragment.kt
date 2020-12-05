b8ca9a5bfa3d67da61c40ddffba3e41e8592b86c
YEJIN-LILY <gristar98@naver.com>
2020-12-05 14:09:49 +0900

    탭 수정

--- app/src/main/java/com/example/androidwithdesign/HomeFragment.kt
+++ app/src/main/java/com/example/androidwithdesign/HomeFragment.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.android.synthetic.main.fragment_home.view.*
+
 
 class HomeFragment : Fragment() {
 
@@ -34,15 +36,14 @@ class HomeFragment : Fragment() {
         for (i in 0 until view.tabLayout.tabCount) {
             val tab = (view.tabLayout.getChildAt(0) as ViewGroup).getChildAt(i)
             val p = tab.layoutParams as ViewGroup.MarginLayoutParams
-            p.setMargins(Utils.dpToPx(8), 0, Utils.dpToPx(8), 0)
-            tab.requestLayout()
+            p.setMargins(Utils.dpToPx(9), 0, 0, 0)
         }
 
         view.tabLayout.apply{
-            getTabAt(0)?.text="인기 작품"
-            getTabAt(1)?.text="최신 작품"
-            getTabAt(2)?.text="인기 후기"
-            getTabAt(3)?.text="작가 추천"
+            getTabAt(0)?.text="인기작품"
+            getTabAt(1)?.text="최신작품"
+            getTabAt(2)?.text="인기후기"
+            getTabAt(3)?.text="작가추천"
         }
 
         return view
